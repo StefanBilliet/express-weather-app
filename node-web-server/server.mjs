c42398aca6c384da379fea3dc970743628e9ae79
Stefan Billiet <stefanbilliet@me.com>
2018-01-10 19:50:49 +0100

    initial commit

---
+++ node-web-server/server.mjs
@@ -0,0 +1,54 @@
+import express from 'express';
+import expose from './expose.js';
+import hbs from 'hbs';
+import fs from 'fs';
+import os from 'os';
+const {__dirname} = expose;
+
+const app = express();
+
+hbs.registerPartials(`${__dirname}/views/partials`)
+app.set('view engine', 'hbs');
+app.use(express.static(`${__dirname}/public`));
+
+app.use((request, response, next) => {
+  const logEntry = `${new Date().toString()}: ${request.method} - ${request.url}`;
+  console.log(logEntry);
+  fs.appendFile(`${__dirname}/server.log`, `${logEntry}${os.EOL}`, (error) => {
+    if(error)
+      console.log(error);
+  });
+  next();
+});
+
+// app.use((request, response, next) => {
+//   response.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance'
+//   });
+// });
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: 'Home',
+    welcomeMessage: 'Bello!'
+  });
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    errorMessage: 'Booboo'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening at port 3000');
+});
